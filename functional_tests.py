6390d5fc3897d4012cd41b06e9d13f25d9cb3044
lucho <lhitaliani@gmail.com>
2017-05-29 11:50:07 -0300

    La home page acepta POST y la FT no guarda con exito los post creados

--- functional_tests.py
+++ functional_tests.py
@@ -28,11 +28,14 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use a peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feather' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         self.fail("Test terminado.")
 
