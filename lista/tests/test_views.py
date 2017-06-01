10104e1a4b62221aa23e23e8c00f2ae14aed4ab6
lucho <lhitaliani@gmail.com>
2017-06-01 18:24:27 -0300

    Adjust new list view to do model validation

--- lista/tests/test_views.py
+++ lista/tests/test_views.py
@@ -30,6 +30,18 @@ class NewListTest(TestCase):
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = "You cant have an empty list item"
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
