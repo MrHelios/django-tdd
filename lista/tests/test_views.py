556e7cfe7a66d27e5876e54d1fcc572c30f167cc
lucho <lhitaliani@gmail.com>
2017-06-06 18:15:34 -0300

    Item duplicate in existing list not allow

--- lista/tests/test_views.py
+++ lista/tests/test_views.py
@@ -3,7 +3,7 @@ from unittest import skip
 
 from lista.models import Item, List
 from lista.views import home_page
-from lista.forms import ItemForm, EMPTY_ITEM_ERROR
+from lista.forms import ItemForm, ExistingListItemForm, EMPTY_ITEM_ERROR
 
 class NewListTest(TestCase):
 
@@ -21,11 +21,6 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertContains(response, EMPTY_ITEM_ERROR)
 
-    def test_for_invalid_input_passes_form_to_template(self):
-        response = self.client.post('/lists/new', data={'text':''})
-        self.assertIsInstance(response.context['form'], ItemForm)
-
-
 class ListViewTest(TestCase):
 
     def post_invalid_input(self):
@@ -34,6 +29,10 @@ class ListViewTest(TestCase):
             f'/lists/{list_.id}/',
             data={'text': ''}
         )
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_nothing_saved_to_db(self):
         self.post_invalid_input()
@@ -117,10 +116,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
