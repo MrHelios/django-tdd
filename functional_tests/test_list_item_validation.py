5a2e1996499d06970533a0756ec6ce5b925618de
lucho <lhitaliani@gmail.com>
2017-06-06 18:35:04 -0300

    Add a new feature: error dissapear, FT fail

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,8 +1,12 @@
 from .base import FunctionalTest
+
 from unittest import skip
 from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
+
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.live_server_url)
@@ -36,6 +40,26 @@ class ItemValidationTest(FunctionalTest):
         self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You have already got this in your list"
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_text').send_keys('Banter too thick')
+        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.browser.find_element_by_id('id_text').send_keys('Banter too thick')
+        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        self.browser.find_element_by_id('id_text').send_keys('a')
+
+        # Cuando empezas a escribir nuevamente el mensaje se borra.
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
